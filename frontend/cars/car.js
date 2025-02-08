241e58213629d0b482cc0138bff2993a175f81fe
Erkhemtugs <erhemevlee@gmail.com>
2025-02-08 01:04:13 -0600

    car img is bigger

--- frontend/cars/car.js
+++ frontend/cars/car.js
@@ -19,31 +19,31 @@ async function fetchCarDetails() {
                     <img src=".${car.image}" alt="${car.make} ${car.name} ${car.year}" loading="lazy" width="440" height="300" class="w-100">
                 </figure>
   
-  <div class="mt-6 border-t border-gray-100">
+  <div class="border-t border-gray-100 p-10">
     <dl class="divide-y divide-gray-100">
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt class="text-sm/6 font-medium text-gray-900">Car Model</dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.make} ${car.name}</dd>
+        <dt class="text-lg font-medium text-gray-900">Car Model</dt>
+        <dd class="mt-1 text-lg text-gray-700 sm:col-span-2 sm:mt-0">${car.make} ${car.name}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt class="text-sm/6 font-medium text-gray-900">Year</dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.year}</dd>
+        <dt class="text-lg font-medium text-gray-900">Year</dt>
+        <dd class="mt-1 text-lg text-gray-700 sm:col-span-2 sm:mt-0">${car.year}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt class="text-sm/6 font-medium text-gray-900">Capacity</dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.capacity}</dd>
+        <dt class="text-lg font-medium text-gray-900">Capacity</dt>
+        <dd class="mt-1 text-lg text-gray-700 sm:col-span-2 sm:mt-0">${car.capacity}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt class="text-sm/6 font-medium text-gray-900">Fuel Type</dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.fuel}</dd>
+        <dt class="text-lg font-medium text-gray-900">Fuel Type</dt>
+        <dd class="mt-1 text-lg text-gray-700 sm:col-span-2 sm:mt-0">${car.fuel}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt class="text-sm/6 font-medium text-gray-900">Consumption</dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.consumption}</dd>
+        <dt class="text-lg font-medium text-gray-900">Consumption</dt>
+        <dd class="mt-1 text-lg text-gray-700 sm:col-span-2 sm:mt-0">${car.consumption}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt class="text-sm/6 font-medium text-gray-900">transmissoin</dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.transmission}</dd>
+        <dt class="text-lg font-medium text-gray-900">transmissoin</dt>
+        <dd class="mt-1 text-lg text-gray-700 sm:col-span-2 sm:mt-0">${car.transmission}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <dt class="text-lg font-medium text-gray-900">Price</dt>
