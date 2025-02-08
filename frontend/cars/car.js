24d0a5a0eb00424b190ca344a0233994294f5ceb
Erkhemtugs <erhemevlee@gmail.com>
2025-02-08 00:57:25 -0600

    Fixed car page details

--- frontend/cars/car.js
+++ frontend/cars/car.js
@@ -18,31 +18,41 @@ async function fetchCarDetails() {
                 <figure class="card-banner">
                     <img src=".${car.image}" alt="${car.make} ${car.name} ${car.year}" loading="lazy" width="440" height="300" class="w-100">
                 </figure>
-                <div class="card-content">
-                    <ul class="card-list">
-                        <li class="card-list-item">
-                            <ion-icon name="people-outline"></ion-icon>
-                            <span class="card-item-text">${car.capacity} Passengers</span>
-                        </li>
-                        <li class="card-list-item">
-                            <ion-icon name="flash-outline"></ion-icon>
-                            <span class="card-item-text">${car.fuel}</span>
-                        </li>
-                        <li class="card-list-item">
-                            <ion-icon name="speedometer-outline"></ion-icon>
-                            <span class="card-item-text">${car.consumption} MPG</span>
-                        </li>
-                        <li class="card-list-item">
-                            <ion-icon name="hardware-chip-outline"></ion-icon>
-                            <span class="card-item-text">${car.transmission}</span>
-                        </li>
-                    </ul>
-                    <div class="card-price-wrapper">
-                        <p class="card-price">
-                            <strong>$${car.price}</strong>
-                        </p>
-                    </div>
-                </div>
+  
+  <div class="mt-6 border-t border-gray-100">
+    <dl class="divide-y divide-gray-100">
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900">Car Model</dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.make} ${car.name}</dd>
+      </div>
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900">Year</dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.year}</dd>
+      </div>
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900">Capacity</dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.capacity}</dd>
+      </div>
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900">Fuel Type</dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.fuel}</dd>
+      </div>
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900">Consumption</dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.consumption}</dd>
+      </div>
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900">transmissoin</dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">${car.transmission}</dd>
+      </div>
+      <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-lg font-medium text-gray-900">Price</dt>
+        <dd class="mt-1 text-xl text-gray-700 sm:col-span-2 sm:mt-0">${car.price}$</dd>
+      </div>
+    </dl>
+  </div>
+</div>
+
             </div>
         `;
         carDetailsContainer.appendChild(carDetail);
